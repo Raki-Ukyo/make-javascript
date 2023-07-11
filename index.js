a41d4991219e5087e20ba33915b55ff9edc6ddb8
Raki-Ukyo <136604155+Raki-Ukyo@users.noreply.github.com>
2023-07-12 04:55:57 +0900

    2 files modified 追加ボタンでアラート

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Import stylesheets
 import './style.css';
+
+const onClickAdd = () => {
+  // 敵視うとボックスの値を取得し初期化する
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = '';
+  alert(inputText);
+};
+
+document
+  .getElementById('add-button')
+  .addEventListener('click', () => onClickAdd());
