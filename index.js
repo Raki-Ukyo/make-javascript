a6d3d281318a79b91033a378e94f8948d7c0dc1e
Raki-Ukyo <136604155+Raki-Ukyo@users.noreply.github.com>
2023-07-12 05:08:44 +0900

    2 files modified 追加ボタン押下後、未完了Todoのリストへテキスト反映

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 import './style.css';
 
 const onClickAdd = () => {
-  // 敵視うとボックスの値を取得し初期化する
+  // テキストボックスの値を取得し初期化する
   const inputText = document.getElementById('add-text').value;
   document.getElementById('add-text').value = '';
-  alert(inputText);
+  // alert(inputText);
+
+  // div生成
+  const div = document.createElement('div');
+  div.className = 'list-row';
+  div.innerText = inputText;
+
+  // liタグ生成
+  const li = document.createElement('li');
+
+  // liの子要素に各要素を設定
+  li.appendChild(div);
+  console.log(li);
+
+  // 未完了リストに追加
+  document.getElementById('incomplete-list').appendChild(li);
 };
 
 document
