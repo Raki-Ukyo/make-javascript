4f0ea1cd5cd1ea0ac775a0f74bb1d6fb30b883ab
Raki-Ukyo <136604155+Raki-Ukyo@users.noreply.github.com>
2023-07-13 05:34:52 +0900

    1 file modified 完了・削除ボタンを押下時のアラート実装およびpタグ内にインプットテキストとなるよう修正

--- index.js
+++ index.js
@@ -5,18 +5,38 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し初期化する
   const inputText = document.getElementById('add-text').value;
   document.getElementById('add-text').value = '';
-  // alert(inputText);
 
   // div生成
   const div = document.createElement('div');
   div.className = 'list-row';
-  div.innerText = inputText;
+
+  //pタグ生成
+  const p = document.createElement('p');
+  p.innerText = inputText;
+
+  // bnutton("完了")タグ生成
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => {
+    alert('完了');
+  });
+
+  // bnutton("削除")タグ生成
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '削除';
+  deleteButton.addEventListener('click', () => {
+    alert('削除');
+  });
 
   // liタグ生成
   const li = document.createElement('li');
 
   // liの子要素に各要素を設定
   li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
   console.log(li);
 
   // 未完了リストに追加
